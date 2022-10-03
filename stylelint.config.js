3ecd23fd0db660b99ae9a5fad15e9edb42748182
Max David <max@the.com>
2022-10-03 12:31:26 -0700

    Add storybook, stylelint

---
+++ stylelint.config.js
@@ -0,0 +1,11 @@
+/** @type {import('stylelint').Config} */
+module.exports = {
+  extends: ['stylelint-config-standard-scss', 'stylelint-config-prettier-scss'],
+  ignoreFiles: ['**/node_modules/**/*.{css,scss}'],
+  rules: {
+    'color-named': ['never', { severity: 'warning' }],
+    'color-no-hex': [true, { severity: 'warning' }],
+    'declaration-no-important': [true, { severity: 'warning' }],
+    'rule-empty-line-before': null,
+  },
+};
