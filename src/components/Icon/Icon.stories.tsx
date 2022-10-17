3cc9327b8f2c868efb1517e589e27dbe02f66805
Max David <max@the.com>
2022-10-17 14:18:48 -0700

    Add Icon story

---
+++ src/components/Icon/Icon.stories.tsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import { Story } from '@storybook/react';
+import { Icon, IconProps, IconSource } from './Icon';
+import { AppIconSet, DocumentIconSet, BrandIconSet } from './icons';
+import { Color, ColorKey } from '../Color';
+
+/**
+ * Helper component for displaying Icon sets in Storybook
+ * @param {IconProps} args \<Icon\> args
+ * @param {IconSource[]} icons icon set
+ */
+const IconGroup = (args: IconProps & { icons: IconSource[] }) => {
+  const { icons, ...iconProps } = args;
+  return (
+    <div
+      style={{
+        display: 'grid',
+        gap: '1rem',
+        gridTemplateColumns: 'repeat(auto-fit, 24px)',
+      }}
+    >
+      {icons.map((icon, i) => (
+        <Icon {...iconProps} source={icon} key={JSON.stringify(i)} />
+      ))}
+    </div>
+  );
+};
+
+export default {
+  title: 'Components/Icon',
+  component: Icon,
+  argTypes: {
+    icons: {
+      table: {
+        disable: true,
+      },
+    },
+    color: {
+      type: 'select',
+      options: ColorKey,
+    },
+    source: {
+      control: false,
+    },
+  },
+};
+
+const TemplateIconGroup: Story<IconProps & { icons: IconSource[] }> = (
+  args
+) => <IconGroup {...args} key={JSON.stringify(args)} />;
+
+const defaultArgs: Partial<IconProps> = {
+  color: Color.Default.Dark,
+  size: 'small',
+};
+
+export const AllIcons = TemplateIconGroup.bind({});
+AllIcons.args = {
+  ...defaultArgs,
+  icons: [...AppIconSet, ...DocumentIconSet, ...BrandIconSet],
+};
+
+export const App = TemplateIconGroup.bind({});
+App.args = {
+  ...defaultArgs,
+  icons: AppIconSet,
+};
+
+export const Document = TemplateIconGroup.bind({});
+Document.args = {
+  ...defaultArgs,
+  icons: DocumentIconSet,
+};
+
+export const Brand = TemplateIconGroup.bind({});
+Brand.args = {
+  ...defaultArgs,
+  icons: BrandIconSet,
+};
---
+++ src/components/Icon/icons/sets.ts
@@ -0,0 +1,81 @@
+import { default as Add } from '../raw/Add.svg';
+import { default as ArrowRight } from '../raw/ArrowRight.svg';
+import { default as AutomatePage } from '../raw/Automate Page.svg';
+import { default as Automate } from '../raw/Automate.svg';
+import { default as Block } from '../raw/Block.svg';
+import { default as Calendar } from '../raw/Calendar.svg';
+import { default as CaretDown } from '../raw/CaretDown.svg';
+import { default as CaretLeft } from '../raw/CaretLeft.svg';
+import { default as CaretRight } from '../raw/CaretRight.svg';
+import { default as CaretUp } from '../raw/CaretUp.svg';
+import { default as ChevronDown } from '../raw/ChevronDown.svg';
+import { default as ChevronLeft } from '../raw/ChevronLeft.svg';
+import { default as ChevronRight } from '../raw/ChevronRight.svg';
+import { default as ChevronUp } from '../raw/ChevronUp.svg';
+import { default as Close } from '../raw/Close.svg';
+import { default as DefaultSheets } from '../raw/Default Sheets.svg';
+import { default as Delete } from '../raw/Delete.svg';
+import { default as Duplicate } from '../raw/Duplicate.svg';
+import { default as Edit } from '../raw/Edit.svg';
+import { default as FontSheet } from '../raw/Font Sheet.svg';
+import { default as Home } from '../raw/Home.svg';
+import { default as Inspect } from '../raw/Inspect.svg';
+import { default as Link } from '../raw/Link.svg';
+import { default as Locked } from '../raw/Locked.svg';
+import { default as More } from '../raw/More.svg';
+import { default as Open } from '../raw/Open.svg';
+import { default as Page } from '../raw/Page.svg';
+import { default as Play } from '../raw/Play.svg';
+import { default as Refresh } from '../raw/Refresh.svg';
+import { default as Responsive } from '../raw/Responsive.svg';
+import { default as Search } from '../raw/Search.svg';
+import { default as Sheet } from '../raw/Sheet.svg';
+import { default as Tetra } from '../raw/Tetra.svg';
+import { default as Unlocked } from '../raw/Unlocked.svg';
+import { default as UX } from '../raw/UX.svg';
+import { default as VersionHistory } from '../raw/Version History.svg';
+import { default as View } from '../raw/View.svg';
+import { default as ViewOff } from '../raw/ViewOff.svg';
+
+export const AppIconSet = [
+  Add,
+  ArrowRight,
+  Automate,
+  Calendar,
+  CaretDown,
+  CaretLeft,
+  CaretRight,
+  CaretUp,
+  ChevronDown,
+  ChevronLeft,
+  ChevronRight,
+  ChevronUp,
+  Close,
+  Delete,
+  Duplicate,
+  Edit,
+  Home,
+  Inspect,
+  Link,
+  Locked,
+  More,
+  Open,
+  Play,
+  Refresh,
+  Responsive,
+  Search,
+  Unlocked,
+  UX,
+  VersionHistory,
+  View,
+  ViewOff,
+];
+export const DocumentIconSet = [
+  AutomatePage,
+  Block,
+  DefaultSheets,
+  FontSheet,
+  Page,
+  Sheet,
+];
+export const BrandIconSet = [Tetra];
